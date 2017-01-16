f705767d426cf11e1f926caaf7af7b1d5ac534d9
soichit <soichit@uw.edu>
2017-01-16 03:28:52 -0800

    finished AJAX

--- search.php
+++ search.php
@@ -1,17 +1,22 @@
 
+<!-- <?php
+	 //$title=$_POST["title"];
+	 //echo $title;
+ ?> -->
+
 
 <?php
 	require_once('player.php');
 
 	// load index.html file
-	$html = file_get_contents('index.html');
-	echo $html;
+	// $html = file_get_contents('index.html');
+	// echo $html;
 
 
 
 
 	// Business logic that retrieves data from RDS based on user input
-	if (isset($_POST["player"]) && !empty($_POST["player"])) {
+	if (isset($_POST["title"]) && !empty($_POST["title"])) {
 
 		// Connect to RDS
 		$servername = "mydbinstance.cclthmosu4ny.us-west-2.rds.amazonaws.com";
@@ -28,11 +33,11 @@
 		}
 
 		$myArray = [];
-		if (!filter_input(INPUT_POST, "player", FILTER_DEFAULT)) {
+		if (!filter_input(INPUT_POST, "title", FILTER_DEFAULT)) {
 	 	   echo("<h3>Your search is not valid</h3>");
 		}
-		$input = filter_input(INPUT_POST, "player", FILTER_DEFAULT);
-		$input = preg_replace('/\s+/', '', $input);
+		$input = filter_input(INPUT_POST, "title", FILTER_DEFAULT);
+		//$input = preg_replace('/\s+/', '', $input);
 
 		try {
 		    $query = $conn->prepare("SELECT * FROM PLAYERS1 WHERE Name LIKE  :input");
@@ -65,43 +70,44 @@
 					"<h2>" . $player->getName() . "</h2>" .
 					"<h4>Team: " . $player->getTeam() . " | GP: " . $player->getGP() . " | Min: " . $player->getMin() . "</h4><hr />" .
 
-					"<div class='box1'/>" .
-						"<h5>FG</h5>" .
+					"<span class='box1'>" .
+						"<h5>FG</h5><br />" .
 						"<table border='1'><tr><td>M</td><td>A</td><td>PCT</td></tr>" .
 						"<tr><td>" . $player->getFG_M() . "</td><td>" . $player->getFG_A() . "</td><td>" . $player->getFG_Pct() . "</td>" .
 						"</tr></table>" .
+					"</span>" .
 
-						"<h5>Three PT</h5>" .
+					"<span class='box1'>" .
+						"<h5>Three PT</h5><br />" .
 						"<table class='right' border='1'><tr><td>M</td><td>A</td><td>Pct</td></tr>" .
 						"<tr><td>" . $player->getThree_PT_M() . "</td><td>" . $player->getThree_PT_A() . "</td><td>" . $player->getThree_PT_Pct() . "</td>" .
 						"</tr></table>" .
-					"</div>" .
+					"</span>" .
 
-					"<div class='box1'/>" .
-						"<h5>FT</h5>" .
+					"<span class='box1'>" .
+						"<h5>FT</h5><br />" .
 						"<table class='right' border='1'><tr><td>M</td><td>A</td><td>Pct</td></tr>" .
 						"<tr><td>" . $player->getFT_M() . "</td><td>" . $player->getFT_A() . "</td><td>" . $player->getFT_Pct() . "</td>" .
 						"</tr></table>" .
-
-						"<h5>Rebounds</h5>" .
+					"</span>" .
+					"<span class='box1'>" .
+						"<h5>Rebounds</h5><br />" .
 						"<table class='right' border='1'><tr><td>Off</td><td>Def</td><td>Tot</td></tr>" .
 						"<tr><td>" . $player->getRebounds_Off() . "</td><td>" . $player->getRebounds_Def() . "</td><td>" . $player->getRebounds_Tot() . "</td>" .
 						"</tr></table>" .
-				    "</div>" .
-
-					"<div class='box1'/>" .
-						"<h5>Misc</h5>" .
+				    "</span>" .
+					"<span class='box1'>" .
+						"<h5>Misc</h5><br />" .
 						"<table class='right' border='1'><tr><td>Ast</td><td>TO</td><td>Stl</td><td>Blk</td><td>PF</td><td>PPG</td></tr>" .
 						"<tr><td>" . $player->getMisc_Ast() . "</td><td>" . $player->getMisc_TO() . "</td><td>" . $player->getMisc_Stl() . "</td>" .
 						"<td>" . $player->getMisc_Blk() . "</td><td>" . $player->getMisc_PF() . "</td><td>" . $player->getMisc_PPG() . "</td>" .
 						"</tr></table>" .
-					"</div>" .
+					"</span>" .
 				"</div>";
 			}
 		}
 	}
 ?>
-
 
 
 
